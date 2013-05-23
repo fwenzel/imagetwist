0b5efd4533ce76ae62dac0653cb078acaa97e2c7
Fred Wenzel <fwenzel@mozilla.com>
2013-05-22 21:52:06 -0700

    Added some info and a link to revert when auto-rotating an image.

--- data/js/replace_image.js
+++ data/js/replace_image.js
@@ -23,15 +23,48 @@ self.port.on('replace', function(msg) {
         node.removeAttribute('data-imagetwist-jobid'); // Discard job ID.
     }
 
-    if (dir % 2) {
-        // 90 or 270 degree turn: Flip width and height.
-        var new_height = node.width;
-        var new_width = node.height;
-        node.height = new_height;
-        node.width = new_width;
+    if (dir % 2) {  // 90 or 270 degree turn: Flip width and height.
+        flipDimensions(node);
     }
 
     node.src = new_src;
 
+    // On a single-image page, allow to revert.
+    if (job_id === null) {
+        var style = document.createElement('style');
+        var p = document.createElement('p');
+
+        style.innerHTML = 'p {' +
+            'color: #fff; font: 10pt sans-serif; position: absolute;' +
+            'bottom: 0; right: .5em; }' +
+            'a { color: #fff; padding: .2em; }' +
+            'a#revert { font-size: 150%; margin: 0 1em; }';
+        p.innerHTML = 'Image was automatically rotated.<sup><a href="' +
+            'https://addons.mozilla.org/addon/imagetwist/">(?)</a></sup> ' +
+            '<a href="' + window.location.href + '" id="revert">&#10008;</a>';
+        document.querySelector('head').appendChild(style);
+        document.querySelector('body').appendChild(p);
+
+        document.querySelector('a#revert').addEventListener('click', function(e) {
+            e.preventDefault();
+            var img = document.images[0];
+            img.src = window.location.href;
+            if (dir % 2) {  // Flip dimensions back if needed.
+                flipDimensions(img);
+            }
+            document.querySelector('body').removeChild(p);
+        });
+    }
+
     self.port.emit('destroyme');  // We don't need this pagemod any longer.
 });
+
+/**
+ * Flip image width and height.
+ */
+function flipDimensions(img) {
+    var new_height = img.width;
+    var new_width = img.height;
+    img.height = new_height;
+    img.width = new_width;
+}
